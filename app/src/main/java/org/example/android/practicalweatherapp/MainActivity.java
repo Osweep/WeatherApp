fb08e6dc806038bf2fde232051969ad055ea22ad
Museh <Ho-yin@live.com>
2015-06-01 19:32:59 +0200

    Kleine aanpassing: buttons verwisseld, toevoeging titel van weather guide.

--- app/src/main/java/org/example/android/practicalweatherapp/MainActivity.java
+++ app/src/main/java/org/example/android/practicalweatherapp/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
             mBtn1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), Cirrocumulus.class);
+                    Intent intent = new Intent(getApplicationContext(), Cirrus.class);
                     startActivity(intent);
                 }
             });
@@ -31,7 +31,7 @@ public class MainActivity extends ActionBarActivity {
             mBtn2.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(getApplicationContext(), Cirrus.class);
+                    Intent intent = new Intent(getApplicationContext(), Cirrocumulus.class);
                     startActivity(intent);
                 }
             });
